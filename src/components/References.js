25587fd0a46235c60e3d1fda56a5158d164e7d27
Tim Jackson <timjacksonm@gmail.com>
2021-04-05 17:20:29 -0500

    added fields to references section.

--- src/components/References.js
+++ src/components/References.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class References extends Component {
-	render() {
-		return (
-			<div>
-				<p>
-					Reference information. John Doe / title / phone # / relation. etc.
-				</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h3>References</h3>
+        <form>
+          <h5>Name</h5>
+          <input type='text' />
+          <h5>Relation</h5>
+          <input type='text' />
+          <h5>Phone Number</h5>
+          <input type='tel' />
+          <button>Add</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default References;
