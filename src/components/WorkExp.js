169cd27132db1605a85168b1f9ab4b2154408eab
Tim Jackson <timjacksonm@gmail.com>
2021-04-05 17:16:53 -0500

    added fields to the work experience section.

--- src/components/WorkExp.js
+++ src/components/WorkExp.js
@@ -1,16 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class WorkExp extends Component {
-	render() {
-		return (
-			<div>
-				<p>
-					Work Experience information. ie. Operations expert at Company Name.
-					Job duties include : Work on CRM application. etc.
-				</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h3>Your Work Experience</h3>
+        <form>
+          <h5>Company Name</h5>
+          <input type='text' />
+          <h5>Job Title</h5>
+          <input type='text' />
+          <h5>Job Location</h5>
+          <input type='text' />
+          <h5>Start Date</h5>
+          <input type='date' />
+          <h5>End Date</h5>
+          <input type='date' />
+
+          <h5>Job Responsibilities</h5>
+          <input type='text' />
+          <button>Add</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default WorkExp;
