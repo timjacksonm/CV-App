995ddefbb4d0012e8f4329fbba7f0eb8ffe9b607
Tim Jackson <timjacksonm@gmail.com>
2021-04-05 16:49:44 -0500

    added input fields to Personal Information section.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,11 @@ class Profile extends Component {
 	render() {
 		return (
 			<div>
-				<p>Profile information ie. name, email, phone #</p>
+				<h3>Personal Information</h3>
+				<input placeholder="First Name" />
+				<input placeholder="Last Name" />
+				<input placeholder="Email" />
+				<input placeholder="Phone Number" type="tel" />
 			</div>
 		);
 	}
