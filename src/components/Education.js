058b7815f49827c6ce70e6bb84ca877041f05a75
Tim Jackson <timjacksonm@gmail.com>
2021-08-28 20:30:13 -0500

    destructured props on education.js

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,73 +4,82 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/Education.scss';
 
 const Education = (props) => {
+  const { onAdd, handleChange, educationList, onDelete } = props;
+  const {
+    eduNameInput,
+    eduLocationInput,
+    eduDegreeInput,
+    eduMajorInput,
+    eduDateInput,
+  } = props.inputState;
   return (
-    <div className='education'>
+    <div className="education">
       <h3>Education and Training</h3>
-      <form id='education' className='formQuestions' onSubmit={props.onAdd}>
+      <form id="education" className="formQuestions" onSubmit={onAdd}>
         <h5>School Name</h5>
         <input
-          type='text'
-          placeholder='Stanford University'
-          name='eduNameInput'
-          onChange={props.handleChange}
-          value={props.inputState.eduNameInput}
+          type="text"
+          placeholder="Stanford University"
+          name="eduNameInput"
+          onChange={handleChange}
+          value={eduNameInput}
           required
         />
         <h5>School Location</h5>
         <input
-          type='text'
-          placeholder='Standford, CA'
-          name='eduLocationInput'
-          onChange={props.handleChange}
-          value={props.inputState.eduLocationInput}
+          type="text"
+          placeholder="Standford, CA"
+          name="eduLocationInput"
+          onChange={handleChange}
+          value={eduLocationInput}
           required
         />
         <h5>Degree</h5>
         <input
-          type='text'
-          placeholder='BS'
-          name='eduDegreeInput'
-          onChange={props.handleChange}
-          value={props.inputState.eduDegreeInput}
+          type="text"
+          placeholder="BS"
+          name="eduDegreeInput"
+          onChange={handleChange}
+          value={eduDegreeInput}
           required
         />
         <h5>Major</h5>
         <input
-          tpye='text'
-          placeholder='Computer Science'
-          name='eduMajorInput'
-          onChange={props.handleChange}
-          value={props.inputState.eduMajorInput}
+          tpye="text"
+          placeholder="Computer Science"
+          name="eduMajorInput"
+          onChange={handleChange}
+          value={eduMajorInput}
           required
         />
         <h5>Completion Date</h5>
         <input
-          type='date'
-          name='eduDateInput'
-          onChange={props.handleChange}
-          value={props.inputState.eduDateInput}
+          type="date"
+          name="eduDateInput"
+          onChange={handleChange}
+          value={eduDateInput}
           required
         />
-        <button type='submit'>Add</button>
+        <button type="submit">Add</button>
       </form>
       <ul>
-        {props.educationList.map((item) => {
+        {educationList.map((item) => {
+          const { Name, Location, Degree, Major, DateCompleted } = item;
           return (
-            <div key={uniqid()} className='educationAdded'>
+            <div key={uniqid()} className="educationAdded">
               <li key={uniqid()}>
                 <ul>
-                  <li>{`Name: ${item.Name}`}</li>
-                  <li>{`Location: ${item.Location}`}</li>
-                  <li>{`Degree: ${item.Degree}`}</li>
-                  <li>{`Major: ${item.Major}`}</li>
-                  <li>{`Completion: ${item.DateCompleted}`}</li>
+                  <li>{`Name: ${Name}`}</li>
+                  <li>{`Location: ${Location}`}</li>
+                  <li>{`Degree: ${Degree}`}</li>
+                  <li>{`Major: ${Major}`}</li>
+                  <li>{`Completion: ${DateCompleted}`}</li>
                 </ul>
               </li>
               <FontAwesomeIcon
-                onClick={() => props.onDelete('education', item.Name)}
+                onClick={() => onDelete('education', Name)}
                 key={uniqid()}
-                icon='trash'
+                icon="trash"
               />
             </div>
           );
