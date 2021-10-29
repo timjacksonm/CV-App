7ab05dee2bf4574733c2969daeb29da02ef818b8
Tim Jackson <timjacksonm@gmail.com>
2021-10-28 20:17:42 -0500

    updated twitter url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
-import { ReactComponent as Github } from "../assets/github-icon.svg";
-import { ReactComponent as Twitter } from "../assets/twitter.svg";
-import "../styles/Footer.scss";
+import React, { Component } from 'react';
+import { ReactComponent as Github } from '../assets/github-icon.svg';
+import { ReactComponent as Twitter } from '../assets/twitter.svg';
+import '../styles/Footer.scss';
 
 class Footer extends Component {
   render() {
     return (
-      <div className='footer'>
+      <div className="footer">
         <div>Created by TJ</div>
-        <div className='logo'>
-          <a href='https://github.com/timjacksonm'>
+        <div className="logo">
+          <a href="https://github.com/timjacksonm">
             <Github />
           </a>
-          <a href='https://twitter.com/TimJacksonMN'>
+          <a href="https://twitter.com/timjacksonm">
             <Twitter />
           </a>
         </div>
